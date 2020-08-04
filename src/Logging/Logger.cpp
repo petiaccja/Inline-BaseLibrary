28359457025d6e8c99c821969af1551fd16e20ce
Peter Kardos <kardospeter1994@hotmail.com>
2020-08-04 23:14:14 +0200

    restructured to a proper library format

---
+++ src/Logging/Logger.cpp
@@ -0,0 +1,55 @@
+#include <InlineLib/Logging/Logger.hpp>
+
+#include <InlineLib/Logging/LogNode.hpp>
+#include <InlineLib/Logging/LogPipe.hpp>
+
+#include <cassert>
+
+
+namespace inl {
+
+
+Logger::Logger() {
+	myNode = std::make_shared<LogNode>();
+	outputFile = std::make_unique<std::ofstream>();
+}
+
+bool Logger::OpenFile(const std::string& path) {
+	std::ofstream newStream(path, std::ios::out | std::ios::trunc);
+	if (!newStream.is_open()) {
+		myNode->SetOutputStream(nullptr);
+		return false;
+	}
+	else {
+		myNode->SetOutputStream(nullptr);
+		outputFile->close();
+		*outputFile = std::move(newStream);
+		myNode->SetOutputStream(outputFile.get());
+		return true;
+	}
+}
+
+void Logger::OpenStream(std::ostream* stream) {
+	myNode->SetOutputStream(stream);
+	outputFile->close();
+}
+
+void Logger::CloseStream() {
+	myNode->SetOutputStream(nullptr);
+	outputFile->close();
+}
+
+LogStream Logger::CreateLogStream(const std::string& name) {
+	std::shared_ptr<LogPipe> pipe(new LogPipe(myNode));
+	myNode->AddPipe(pipe, name);
+	return LoggerInterface::Construct(pipe);
+}
+
+void Logger::Flush() {
+	myNode->Flush();
+}
+
+
+Logger g_logger;
+
+} // namespace inl
