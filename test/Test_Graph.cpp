03291ca46098996d4f6dc4835aeaa04e8a47d72a
Péter Kardos <kardospeter1994@hotmail.com>
2020-08-05 21:31:00 +0200

    added tests for graph

---
+++ test/Test_Graph.cpp
@@ -0,0 +1,30 @@
+﻿#include <InlineLib/Graph.hpp>
+
+#include <Catch2/catch.hpp>
+
+using namespace inl;
+
+
+TEST_CASE("Link ports same type", "[Graph]") {
+	InputPort<float> inputPort;
+	OutputPort<float> outputPort;
+	inputPort.Link(outputPort);
+	REQUIRE_NOTHROW(inputPort.GetLink());
+}
+
+
+TEST_CASE("Link ports incompatible types", "[Graph]") {
+	InputPort<float> inputPort;
+	OutputPort<char*> outputPort;
+	inputPort.Link(outputPort);
+	REQUIRE_THROWS(inputPort.GetLink());
+}
+
+
+TEST_CASE("Link ports incompatible types", "[Graph]") {
+	InputPort<float> inputPort;
+	OutputPort<int> outputPort;
+	inputPort.Link(outputPort);
+	REQUIRE_NOTHROW(inputPort.GetLink());
+}
+
